1308d754e6b401a299746895f13762db112ad4ed
Vittorio Guerriero <vittorio.gue@gmail.com>
2025-06-26 00:22:36 +0100

    refactorings

--- src/main.rs
+++ src/main.rs
@@ -39,29 +39,30 @@ impl Myers {
     fn shortest_edit(&self, a: &Vec<Line>, b: &Vec<Line>) -> usize {
         let n = a.len();
         let m = b.len();
-        let max = (n + m) as i32;
+        let max = n + m;
         let mut x: usize;
         let mut y: usize;
-        let mut v: Vec<usize> = vec![0; (max as usize) * 2 + 1];
+        let mut v: Vec<usize> = vec![0; max * 2 + 1];
 
         v[1] = 0;
 
-        for d in 0..max {
-            for k in (-d..d).step_by(2) {
-                if k == -d || k != d && self.resolve_v(&v, k - 1) < self.resolve_v(&v, k + 1) {
-                    x = self.resolve_v(&v, k + 1);
+        for d in 0..max as i32 {
+            for k2 in (-d..d).step_by(2) {
+                let k = (k2 + max as i32) as usize;
+                if k2 == -d || k2 != d && &v[k - 1] < &v[k + 1] {
+                    x = v[k + 1];
                 } else {
-                    x = self.resolve_v(&v, k - 1) + 1;
+                    x = v[k - 1] + 1;
                 }
 
-                y = (x as i32 - k) as usize;
+                y = (x as i32 - k2) as usize;
 
                 while x < n && y < m && a[x].text == b[y].text {
                     x = x + 1;
                     y = y + 1;
                 }
 
-                self.set_v(&mut v, k, x);
+                v[k] = x;
 
                 if x == n && y == m {
                     return d as usize;
@@ -70,23 +71,6 @@ impl Myers {
         }
 
         unreachable!()
-    }
-
-    fn resolve_v(&self, v: &Vec<usize>, k: i32) -> usize {
-        if k < 0 {
-            v[(v.len() as i32 + k) as usize]
-        } else {
-            v[k as usize]
-        }
-    }
-
-    fn set_v(&self, v: &mut Vec<usize>, k: i32, x: usize) {
-        if k < 0 {
-            let len = v.len();
-            v[(len as i32 + k) as usize] = x;
-        } else {
-            v[k as usize] = x;
-        }
     }
 }
 
@@ -102,6 +86,5 @@ mod tests {
         let result = Myers.shortest_edit(&Diff.lines(a.to_string()), &Diff.lines(b.to_string()));
 
         assert_eq!(result, 5)
-
     }
 }
